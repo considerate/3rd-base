97d565146a73b2008bdd33cb54861cf639ee5188
christoffer <cmoc@kth.se>
2015-03-02 18:45:53 +0100

    Add cowboy example

---
+++ couch_base/src/couch_base_app.erl
@@ -0,0 +1,21 @@
+-module(couch_base_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+
+start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+        	{"/", hello_handler, []}
+        ]}
+    ]),
+    cowboy:start_http(my_http_listener, 100, [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
+	couch_base_sup:start_link().
+	
+
+stop(_State) ->
+	ok.
