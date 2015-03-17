090b7ed250dcb29fe273f728b4daf4b601c29b52
Marcus Larson <sharkskin@n174-p39.kthopen.kth.se>
2015-03-17 16:57:26 +0100

    added auth in handler

--- baseball/src/new_group_handler.erl
+++ baseball/src/new_group_handler.erl
@@ -1,6 +1,17 @@
 -module(new_grop_handler).
 -export([init/2]).
 
+-define (SECRET, <<"This is a very secret secret, do not tell anyone.">>).
+
 -init(Req,Opts) ->
 	Headers = cowboy_req:headers(Req),
+    {_,Auth}=proplists:lookup(<<"Athorization">>,Headers),
+    "Bearer " ++ Token = Auth,
+    case jwt:decode(Token, ?SECRET) of
+		{ok,Data} -> 
+			Username = proplists:get_value(<<"id">>, Data),
+				
+		Error ->
+			Error
+	end.
 	
